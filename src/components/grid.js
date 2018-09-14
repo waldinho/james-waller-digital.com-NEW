8bd501e9407e034d16c64472b34b2287e0f4910e
james-waller-akqa <james.waller@akqa.com>
2018-09-14 14:27:04 +1000

    content added to services page. Need to style this.

--- src/components/grid.js
+++ src/components/grid.js
@@ -6,7 +6,12 @@ class Grid extends Component {
 
         return (
             <div className='grid'>
-                
+                <h1>
+                    {this.props.title}
+                </h1>
+                <div className='grid__content'>
+                    {this.props.text}
+                </div>
             </div>
         );
     }
