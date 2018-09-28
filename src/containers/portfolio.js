a6ad94aae51122c8123e7f3b1e4873dd81c89b50
james-waller-akqa <james.waller@akqa.com>
2018-09-28 15:18:01 +1000

    styling looking way better.

--- src/containers/portfolio.js
+++ src/containers/portfolio.js
@@ -15,62 +15,59 @@ class Portfolio extends Component {
     }
 
     toggleHidden (e) {
-
         const {
             isHidden,
             isActive,
             isRest,
-            currentTarget
         } = this.state
-
         this.setState({
             isHidden: !isHidden,
             isActive: !isActive,
             isRest: !isRest,
             currentTarget: e.target.id
         })
-
     }
 
-    render(e) {
-
+    render() {
         const {
             isHidden,
             isActive,
             isRest,
             currentTarget
         } = this.state
-
-        console.log("currentTarget: ", currentTarget)
         
         const clickedElement = 'show--' + currentTarget
         const hiddenElement = 'hide--' + currentTarget
         const hideRest = 'hide'
+
         const portfolio = getPortfolio()
         const title = portfolio[0].title 
         const columns = portfolio[0].companies.length / 3
-        const portfolioText = portfolio[0].companies.map((item) => { 
 
-            console.log(clickedElement + ' - ' + item.id)
+        const portfolioText = portfolio[0].companies.map((item) => { 
 
             const hidden = (isHidden ? hiddenElement : clickedElement);
             const active = (isActive ? clickedElement : hiddenElement);
             const rest = (isRest ? '' : hideRest);
-
+            
             const blurb = item.text.map((text) => {
                 return (
                     <p>{text.paragraph}</p>
                 )
             })
+
             return ( 
                 <div id={item.id} onClick={this.toggleHidden.bind(this)}>
-                    <div id={item.id} className={'item columns--' + columns + ' portfolio--' + item.id + ' ' + active}>
-                        <div  id={item.id} className={'title--' + rest}><h2 id={item.id}>{item.title}</h2></div>
+
+                    <div className={'item columns--' + columns + ' portfolio--' + item.id + ' ' + active}>
+                        <div className={'title--' + rest}><h2 id={item.id}>{item.title}</h2></div>
                     </div>
-                    <div id={item.id} className={'columns--1--hidden-item portfolio--' + item.id + ' ' + hidden}>
-                        <div id={item.id} className='title'><h2 id={item.id}>{item.title}</h2></div>
-                        <div id={item.id} className='blurb'>{blurb}</div>
+
+                    <div className={'columns--1--hidden-item portfolio--' + item.id + ' ' + hidden}>
+                        <div className='title'><h2 id={item.id}>{item.title}</h2></div>
+                        <div className='blurb'>{blurb}</div>
                     </div>
+
                 </div>
             )
         })
